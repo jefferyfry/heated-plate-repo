209622eb7001a693143409e36d045a2254013a44
Jeff Fry <jeff@jefferyfry.com>
2014-09-08 12:11:30 -0700

    created heated plate interface.

---
+++ heated-plate/src/common/HeatedPlate.java
@@ -0,0 +1,33 @@
+package common;
+
+public interface HeatedPlate {
+	
+	/**
+	 * Initialize the heated plate class.
+	 * @param dimension dimension of the square lattice (number of rows and columns, not counting the edge values)
+	 * @param left edge temperature for left
+	 * @param right edge temperature for right
+	 * @param top edge temperature for top
+	 * @param bottom edge temperature for bottom
+	 */
+	public void initialize(int dimension,double left, double right,double top,double bottom);
+	
+	/**
+	 * Returns the results for the next iteration of the algorithm.  Intended to be used for animation.
+	 * @return 
+	 */
+	public double[][] nextResults();
+	
+	/**
+	 * Check to see if the algorithm has finished.  Intended to be used for the animation.
+	 * @return True if the algorithm is finished.
+	 */
+	public boolean hasNext();
+	
+	/**
+	 * Gets the final results of the algorithm.
+	 * @return
+	 */
+	public double[][] getFinalResults();
+
+}
