f550c761958c811909ae45624de1e22a647dcf6b
aabreu7 <aabreu7@gatech.edu>
2014-09-13 14:16:18 -0400

    Fixed package mixup

--- heated-plate/src/Twfahp/Diffusion.java
+++ heated-plate/src/Twfahp/Diffusion.java
@@ -2,77 +2,40 @@ package Twfahp;
 
 class Diffusion 
 {
-	private class LatticePoint
-	{
-		private double temperature;
-		private LatticePoint up, down, left, right;
-			
-		public LatticePoint(double temperature)
-		{
-			this.temperature = temperature;
-		}	
-	}
-	
-	private LatticePoint oldPlateRoot, newPlateRoot;
+	private Float oldPlate[][];
+	private Float newPlate[][];
 	
 	private int dim;
-	private int leftTemp, rightTemp, topTemp, bottomTemp;
+	private Float left, right, top, bottom;
 	
 	public Diffusion(int dim, int left, int right, int top, int bottom)
 	{
 		this.dim = dim;
 		
-		this.leftTemp = left;
-		this.rightTemp = right;
-		this.topTemp = top;
-		this.bottomTemp = bottom;
+		this.left = (float) left;
+		this.right = (float) right;
+		this.top = (float) top;
+		this.bottom = (float) bottom;
 		
-		oldPlateRoot = initialize(oldPlateRoot);
-		newPlateRoot = initialize(newPlateRoot);
+		oldPlate = new Float[dim + 2][dim + 2];
+		newPlate = new Float[dim + 2][dim + 2];
+		
+		initialize(oldPlate);
+	    initialize(newPlate);
 	    diffuse();
 	    printTable();
 	}
 	
-	public LatticePoint initialize(LatticePoint plateRoot)
+	public void initialize(Float[][] plate)
 	{
-		plateRoot = new LatticePoint(-1);
-		
-		LatticePoint rowIndex = plateRoot;
-		LatticePoint trav1 = rowIndex;
-		LatticePoint trav2;
-		
-		for(int i=0; i < dim+1; i++)
+		for(int i=1; i <= dim; i++)
 		{
-			trav1.right = new LatticePoint(topTemp);
-			trav1.right.left = trav1;
-			trav1 = trav1.right;
-		}
-		
-		for(int i=0; i < dim+1; i++)
-		{
-			trav1 = rowIndex;
-			trav2 = new LatticePoint(leftTemp);
+			plate[i][0] = left;
+			plate[i][dim+1] = right;
 			
-			for(int j=0; j < dim+1; j++)
-			{
-				trav2.up = trav1;
-				trav1.down = trav2;
-				
-				int temp = 0;
-				if(j == dim) temp = rightTemp;
-				if(i == dim) temp = bottomTemp;
-				
-				trav2.right = new LatticePoint(temp);
-				trav2.right.left = trav2;
-				
-				trav1 = trav1.right;
-				trav2 = trav2.right;
-			}
-			
-			rowIndex = rowIndex.down;
+			plate[0][i] = top;
+			plate[dim+1][i] = bottom;
 		}
-		
-		return plateRoot;
 	}
 	
 	public void diffuse()
@@ -80,82 +43,44 @@ class Diffusion
 		int count = 0;
 		
 		do{
-			LatticePoint newPlateRowIndex = newPlateRoot;
-			LatticePoint oldPlateRowIndex = oldPlateRoot;
-			
-			for(int i=0; i < dim; i++)
-			{
-				newPlateRowIndex = newPlateRowIndex.down;
-				oldPlateRowIndex = oldPlateRowIndex.down;
-				LatticePoint travNewPlate = newPlateRowIndex;
-				LatticePoint travOldPlate = oldPlateRowIndex;
-				
-				for(int j=0; j < dim; j++)
-				{
-					travNewPlate = travNewPlate.right;
-					travOldPlate = travOldPlate.right;
-					
-					travNewPlate.temperature = (travOldPlate.left.temperature + travOldPlate.right.temperature +
-							travOldPlate.up.temperature + travOldPlate.down.temperature) / 4.0;
-				}
-			}	
-		     
+			for (int i = 1; i <= dim; i++) 
+		        for (int j = 1; j <= dim; j++) 
+		          newPlate[i][j] = (oldPlate[i + 1][j] + oldPlate[i - 1][j] +
+		                            oldPlate[i][j + 1] + oldPlate[i][j - 1]) / 4.0F;
+		      
 			swap();
-	      
+		      
 		}while(! done() && count++ < 10000);
 	}
 	
 	private boolean done()
 	{
-		LatticePoint newPlateRowIndex = newPlateRoot;
-		LatticePoint oldPlateRowIndex = oldPlateRoot;
-		
-		for(int i=0; i < dim; i++)
-		{
-			newPlateRowIndex = newPlateRowIndex.down;
-			oldPlateRowIndex = oldPlateRowIndex.down;
-			LatticePoint travNewPlate = newPlateRowIndex;
-			LatticePoint travOldPlate = oldPlateRowIndex;
-			
-			for(int j=0; j < dim; j++)
-			{
-				travNewPlate = travNewPlate.right;
-				travOldPlate = travOldPlate.right;
-				
-				if(travNewPlate.temperature - travOldPlate.temperature != 0)
+		for(int i=1; i <= dim; i++)
+			for(int j=1; j <= dim; j++)
+				if(newPlate[i][j] - oldPlate[i][j] != 0)
 					return false;
-			}
-		}	
 					
 		return true;
 	}
 	
 	private void swap()
 	{
-		LatticePoint temp;
+		Float[][] temp;
 		
-		temp = oldPlateRoot;
-		oldPlateRoot = newPlateRoot;
-		newPlateRoot = temp;
+		temp = oldPlate;
+		oldPlate = newPlate;
+		newPlate = temp;
 	}
 	
 	public void printTable()
 	{
-		LatticePoint printRow = newPlateRoot;
-		
-		for(int i=0; i < dim; i++)
+		for(int i=1; i <= dim; i++)
 		{
-			printRow = printRow.down;
-			LatticePoint printTrav = printRow;
-			
-			for(int j=0; j < dim; j++)
-			{
-				printTrav = printTrav.right;
-				System.out.format("%6.2f ", printTrav.temperature);
-			}
+			for(int j=1; j <= dim; j++)
+				System.out.format("%6.2f ", newPlate[i][j]);
 			
 			System.out.println();
-		}	
+		}
 	}
 	 
 }
