f12a94cab944c0c0c1175dcb0150f65605dd6e6a
aabreu7 <aabreu7@gatech.edu>
2014-09-17 02:01:55 -0400

    Updated main to accept arguments in any order

--- heated-plate/src/Tpfahp/Demo.java
+++ heated-plate/src/Tpfahp/Demo.java
@@ -2,14 +2,27 @@ package Tpfahp;
 
 public class Demo 
 {
-	public static void main(String...args) 
+	public static void main(String...args) throws Exception 
 	{
-		int dim = Integer.parseInt(args[1]);
-		int left = Integer.parseInt(args[3]), 
-			right = Integer.parseInt(args[5]), 
-			top = Integer.parseInt(args[7]), 
-			bottom = Integer.parseInt(args[9]); 
+		int dim = 0;
+		int left = 0, right = 0, top = 0, bottom = 0;
 		
+		for(int i=0; i < args.length; i+=2)
+		{
+			if(args[i].compareToIgnoreCase("-d") == 0)
+				dim = Integer.parseInt(args[i+1]);
+			else if(args[i].compareToIgnoreCase("-l") == 0)
+				left = Integer.parseInt(args[i+1]);
+			else if(args[i].compareToIgnoreCase("-r") == 0)
+				right = Integer.parseInt(args[i+1]);
+			else if(args[i].compareToIgnoreCase("-t") == 0)
+				top = Integer.parseInt(args[i+1]);
+			else if(args[i].compareToIgnoreCase("-b") == 0)
+				bottom = Integer.parseInt(args[i+1]);
+			else
+				throw new Exception("Argument flag is not recognized");
+		}
+	
 		Diffusion diff = new Diffusion(dim, left, right, top, bottom);
 	}
 }
